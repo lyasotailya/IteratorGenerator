c9bfd0782553003a95e112e2d23f1e403d615d17
lyasotailya <lysota2014@yandex.ru>
2023-12-16 20:38:15 +0500

    third

--- test3.py
+++ test3.py
@@ -1,6 +1,6 @@
-class F:
+class FlatIterator:
     def __init__(self, li):
-        self.stack = li
+        self.stack = li[::-1]
 
     def __iter__(self):
         return self
@@ -15,11 +15,23 @@ class F:
         raise StopIteration
 
 
-list_of_lists_2 = [
+def test_3():
+
+    list_of_lists_2 = [
         [['a'], ['b', 'c']],
         ['d', 'e', [['f'], 'h'], False],
         [1, 2, None, [[[[['!']]]]], []]
-    ]
+                       ]
 
-for i in F(list_of_lists_2):
-    print(i)
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_3()
